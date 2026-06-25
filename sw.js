43bf7e2073d5f3f0de6b6c2ad3eff080f635f454
Mayank Mundra <may.c.lnw1432@gmail.com>
2026-06-25 17:12:15 +0530

    feat: add external pwa setup

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'protein-planner-v0.5.0';
+
+const APP_SHELL = [
+  './',
+  './index.html',
+  './js/storage.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(names=>Promise.all(names.filter(name=>name!==CACHE_NAME).map(name=>caches.delete(name))))
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+});
